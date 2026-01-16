0fe97711d6f82a31841b824a5296d8b13e7dc064
Patrick Towles <james.towles@gmail.com>
2026-01-15 21:47:35 -0500

    fix: update shadows_enabled to shadow_maps_enabled for Bevy 0.19-dev
    DirectionalLight.shadows_enabled was renamed to shadow_maps_enabled in bevy main.

--- examples/many_foxes.rs
+++ examples/many_foxes.rs
@@ -220,7 +220,7 @@ fn setup(
     commands.spawn((
         Transform::from_rotation(Quat::from_euler(EulerRot::ZYX, 0.0, 1.0, -PI / 4.)),
         DirectionalLight {
-            shadows_enabled: false,
+            shadow_maps_enabled: false,
             ..default()
         },
     ));
